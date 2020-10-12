08d9c58e60267a3bcbf0777e2942b06ee602504a
Shivam Chaturvedi <shivam@cere.io>
2020-10-13 00:10:30 +0530

    Removal of redundant test pallet.

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -9,10 +9,28 @@
 /// For more guidance on Substrate FRAME, see the example pallet
 /// https://github.com/paritytech/substrate/blob/master/frame/example/src/lib.rs
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, debug, traits::Get};
-use frame_system::{self as system, ensure_signed};
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, debug, traits::Get,
+	dispatch::DispatchResult,
+};
+
+use frame_system::{self as system, ensure_signed,
+	ensure_none,
+	offchain::{
+		AppCrypto, CreateSignedTransaction, SendSignedTransaction, SendUnsignedTransaction,
+		SignedPayload, SigningTypes, Signer, SubmitTransaction,
+	},
+};
 use frame_support::sp_runtime::{print, offchain::http};
 use frame_support::traits::Vec;
+use core::{convert::TryInto};
+use sp_std::{
+	prelude::*, str,
+	collections::vec_deque::VecDeque,
+};
+use sp_core::crypto::KeyTypeId;
+
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"demo");
+pub const NUM_VEC_LEN: usize = 10;
 
 #[cfg(test)]
 mod mock;
@@ -20,10 +38,44 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-/// The pallet's configuration trait.
-pub trait Trait: system::Trait {
-	// Add other types and constants required to configure this pallet.
+/// Based on the above `KeyTypeId` we need to generate a pallet-specific crypto type wrapper.
+/// We can utilize the supported crypto kinds (`sr25519`, `ed25519` and `ecdsa`) and augment
+/// them with the pallet-specific identifier.
+pub mod crypto {
+	use crate::KEY_TYPE;
+	use sp_core::sr25519::Signature as Sr25519Signature;
+	use sp_runtime::app_crypto::{app_crypto, sr25519};
+	use sp_runtime::{
+		traits::Verify,
+		MultiSignature, MultiSigner,
+	};
 
+	app_crypto!(sr25519, KEY_TYPE);
+
+	pub struct TestAuthId;
+	// implemented for ocw-runtime
+	impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
+		type RuntimeAppPublic = Public;
+		type GenericSignature = sp_core::sr25519::Signature;
+		type GenericPublic = sp_core::sr25519::Public;
+	}
+
+	// implemented for mock runtime in test
+	impl frame_system::offchain::AppCrypto<<Sr25519Signature as Verify>::Signer, Sr25519Signature>
+		for TestAuthId
+	{
+		type RuntimeAppPublic = Public;
+		type GenericSignature = sp_core::sr25519::Signature;
+		type GenericPublic = sp_core::sr25519::Public;
+	}
+}
+
+/// This is the pallet's configuration trait
+pub trait Trait: system::Trait + CreateSignedTransaction<Call<Self>> {
+	/// The identifier type for an offchain worker.
+	type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
+	/// The overarching dispatch call type.
+	type Call: From<Call<Self>>;
 	/// The overarching event type.
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
@@ -38,6 +90,9 @@ decl_storage! {
 		// Here we are declaring a StorageValue, `Something` as a Option<u32>
 		// `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
 		Something get(fn something): Option<u32>;
+
+		/// A vector of recently submitted numbers. Bounded by NUM_VEC_LEN
+		Numbers get(fn numbers): VecDeque<u64>;
 	}
 }
 
@@ -48,6 +103,8 @@ decl_event!(
 		/// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
 		/// To emit this event, we call the deposit function, from our runtime functions
 		SomethingStored(u32, AccountId),
+		/// Event generated when a new number is accepted to contribute to the average.
+		NewNumber(Option<AccountId>, u64),
 	}
 );
 
@@ -58,6 +115,11 @@ decl_error! {
 		NoneValue,
 		/// Value reached maximum and cannot be incremented further
 		StorageOverflow,
+		// Error returned when making signed transactions in off-chain worker
+		NoLocalAcctForSigning,
+		OffchainSignedTxError,
+		// Error returned when making unsigned transactions in off-chain worker
+		OffchainUnsignedTxError,
 	}
 }
 
@@ -73,6 +135,26 @@ decl_module! {
 		// Initializing events
 		// this is needed only if you are using events in your pallet
 		fn deposit_event() = default;
+
+		#[weight = 10000]
+		pub fn submit_number_signed(origin, number: u64) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			debug::info!("submit_number_signed: ({}, {:?})", number, who);
+			Self::append_or_replace_number(number);
+
+			Self::deposit_event(RawEvent::NewNumber(Some(who), number));
+			Ok(())
+		}
+
+		#[weight = 10000]
+		pub fn submit_number_unsigned(origin, number: u64) -> DispatchResult {
+			let _ = ensure_none(origin)?;
+			debug::info!("submit_number_unsigned: {}", number);
+			Self::append_or_replace_number(number);
+
+			Self::deposit_event(RawEvent::NewNumber(None, number));
+			Ok(())
+		}
 
 		/// Just a dummy entry point.
 		/// function that can be called by the external world as an extrinsics call
@@ -99,7 +181,10 @@ decl_module! {
 				Err(e) => debug::error!("Error fetch_data: {}", e),
 			  };
 			
-			// NOW I am do this
+			// NOW I am do this, error here: i am fixing compile
+			// Self::offchain_unsigned_tx(block);
+			Self::offchain_signed_tx(block);
+
 			// T::SubmitSignedTransaction::submit_signed(call);
 		}
 		/// Another dummy entry point.
@@ -142,5 +227,64 @@ impl<T: Trait> Module<T> {
 	  let return_val = response.body().collect::<Vec<u8>>();
 	  // Collect the result in the form of bytes
 	  Ok(return_val)
+	}
+
+	/// Append a new number to the tail of the list, removing an element from the head if reaching
+	///   the bounded length.
+	fn append_or_replace_number(number: u64) {
+		Numbers::mutate(|numbers| {
+			if numbers.len() == NUM_VEC_LEN {
+				let _ = numbers.pop_front();
+			}
+			numbers.push_back(number);
+			debug::info!("Number vector: {:?}", numbers);
+		});
+	}
+
+	fn offchain_unsigned_tx(block_number: T::BlockNumber) -> Result<(), Error<T>> {
+		let number: u64 = block_number.try_into().unwrap_or(0) as u64;
+		let call = Call::submit_number_unsigned(number);
+
+		// `submit_unsigned_transaction` returns a type of `Result<(), ()>`
+		//   ref: https://substrate.dev/rustdocs/v2.0.0/frame_system/offchain/struct.SubmitTransaction.html#method.submit_unsigned_transaction
+		SubmitTransaction::<T, Call<T>>::submit_unsigned_transaction(call.into())
+			.map_err(|_| {
+				debug::error!("Failed in offchain_unsigned_tx");
+				<Error<T>>::OffchainUnsignedTxError
+			})
+	}
+
+	fn offchain_signed_tx(block_number: T::BlockNumber) -> Result<(), Error<T>> {
+		// We retrieve a signer and check if it is valid.
+		//   Since this pallet only has one key in the keystore. We use `any_account()1 to
+		//   retrieve it. If there are multiple keys and we want to pinpoint it, `with_filter()` can be chained,
+		//   ref: https://substrate.dev/rustdocs/v2.0.0/frame_system/offchain/struct.Signer.html
+		let signer = Signer::<T, T::AuthorityId>::any_account();
+
+		// Translating the current block number to number and submit it on-chain
+		let number: u64 = block_number.try_into().unwrap_or(0) as u64;
+
+		// `result` is in the type of `Option<(Account<T>, Result<(), ()>)>`. It is:
+		//   - `None`: no account is available for sending transaction
+		//   - `Some((account, Ok(())))`: transaction is successfully sent
+		//   - `Some((account, Err(())))`: error occured when sending the transaction
+		let result = signer.send_signed_transaction(|_acct|
+			// This is the on-chain function
+			Call::submit_number_signed(number)
+		);
+
+		// Display error if the signed tx fails.
+		if let Some((acc, res)) = result {
+			if res.is_err() {
+				debug::error!("failure: offchain_signed_tx: tx sent: {:?}", acc.id);
+				return Err(<Error<T>>::OffchainSignedTxError);
+			}
+			// Transaction is sent successfully
+			return Ok(());
+		}
+
+		// The case of `None`: no account is available for sending
+		debug::error!("No local account available");
+		Err(<Error<T>>::NoLocalAcctForSigning)
 	}
   }
